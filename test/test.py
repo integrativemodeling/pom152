71c5afc37e7e13edbd2ebadf63c7f6cbd84d519a
Ben Webb <ben@salilab.org>
2018-07-24 12:50:34 -0700

    Use ihm library to read in and test output mmCIF

--- test/test.py
+++ test/test.py
@@ -5,6 +5,7 @@ import os
 import shutil
 import sys
 import subprocess
+import ihm.reader
 
 TOPDIR = os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]), '..'))
 
@@ -72,10 +73,53 @@ class Tests(unittest.TestCase):
         p = subprocess.check_call(
                 ["python", "modeling_pdb375_482.py", "--mmcif=pom152.cif",
                  "--dry-run"], env=env)
-        # Check size of output file
-        with open("pom152.cif") as fh:
-            wcl = len(fh.readlines())
-        self.assertTrue(wcl >= 10980)
+        # Check output file
+        self._check_mmcif_file('pom152.cif')
+
+    def _check_mmcif_file(self, fname):
+        with open(fname) as fh:
+            s, = ihm.reader.read(fh)
+        self.assertEqual(len(s.citations), 1)
+        self.assertEqual(s.citations[0].doi, '10.1016/j.str.2017.01.006')
+        self.assertEqual(len(s.software), 3)
+        self.assertEqual(len(s.orphan_starting_models), 6)
+        # Should be a single model in a single state
+        self.assertEqual(len(s.state_groups), 1)
+        self.assertEqual(len(s.state_groups[0]), 1)
+        self.assertEqual(len(s.state_groups[0][0]), 1)
+        model = s.state_groups[0][0][0][0]
+        self.assertEqual(len(model._atoms), 0)
+        self.assertEqual(len(model._spheres), 882)
+        # Should be 1 ensemble (cluster)
+        self.assertEqual([e.num_models for e in s.ensembles], [364])
+        # Check localization densities
+        self.assertEqual([len(e.densities) for e in s.ensembles], [10])
+        self.assertEqual([len(e.sequence) for e in s.entities], [1337])
+        self.assertEqual([a.details for a in s.asym_units], ['pom152'])
+        # 14 restraints - EM3D, 8 EM2D images and 5 SAXS profiles
+        self.assertEqual(len(s.restraints), 14)
+        em3d = s.restraints[0]
+        self.assertEqual(em3d.dataset.location.path,
+                         'data/pom152_relion_s40.gmm.50.txt')
+        self.assertEqual(em3d.dataset.parents[0].location.access_code,
+                         'EMD-8543')
+
+        em2d_rsr = s.restraints[1:-5]
+        for i, em2d in enumerate(em2d_rsr):
+            if i in (1, 3):
+                self.assertAlmostEqual(em2d.image_resolution, 60.0, places=1)
+            else:
+                self.assertAlmostEqual(em2d.image_resolution, 50.0, places=1)
+            # We don't know the fit or # of micrographs
+            self.assertEqual(em2d.number_raw_micrographs, None)
+            self.assertEqual(len(em2d.fits), 0)
+
+        sas_rsr = s.restraints[-5:]
+        for sas in sas_rsr:
+            self.assertEqual(sas.segment, False)
+            self.assertEqual(sas.fitting_method, "FoXS")
+            self.assertEqual(sas.multi_state, False)
+            self.assertEqual(sas.number_of_gaussians, None)
 
     def test_simple(self):
         """Test model building and analysis"""
